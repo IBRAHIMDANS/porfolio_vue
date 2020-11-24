21bd74e09d30bdc18095339988e12f27937a4ade
Ibrahima DANSOKO <ibrahima.Dansoko@outlook.com>
2020-11-24 13:00:44 +0100

    porfolio vue

--- src/main.js
+++ src/main.js
@@ -1,5 +1,23 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
 
-createApp(App).use(router).mount('#app')
+
+import "./assets/style/main.scss";
+import "vue-material/dist/vue-material.min.css";
+import "vue-material/dist/theme/default.css";
+
+// import { MdButton, MdContent, MdTabs } from "vue-material";
+
+
+const app = createApp(App);
+
+// app.use(MdButton);
+// app.use(MdContent);
+// app.use(MdTabs);
+
+app.use(router);
+
+// app.component('base-button', BaseButton)
+
+app.mount("#app");
